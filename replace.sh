3b72428843c69ce40f617b955b55a83bf8cebbdc
wieczorek1990 <wieczorek1990@gmail.com>
2013-10-26 17:31:08 +0200

    more years

--- replace.sh
+++ replace.sh
@@ -9,9 +9,19 @@ mkdir out
 ./replace.rb rok3.txt out/bez_poznania_z_danymi.lp out/bp3.lp
 ./replace.rb rok4.txt out/bez_poznania_z_danymi.lp out/bp4.lp
 ./replace.rb rok5.txt out/bez_poznania_z_danymi.lp out/bp5.lp
+./replace.rb rok6.txt out/bez_poznania_z_danymi.lp out/bp6.lp
+./replace.rb rok7.txt out/bez_poznania_z_danymi.lp out/bp7.lp
+./replace.rb rok8.txt out/bez_poznania_z_danymi.lp out/bp8.lp
+./replace.rb rok9.txt out/bez_poznania_z_danymi.lp out/bp9.lp
+./replace.rb rok10.txt out/bez_poznania_z_danymi.lp out/bp10.lp
 
 ./replace.rb rok1.txt out/z_poznaniem_z_danymi.lp out/zp1.lp
 ./replace.rb rok2.txt out/z_poznaniem_z_danymi.lp out/zp2.lp
 ./replace.rb rok3.txt out/z_poznaniem_z_danymi.lp out/zp3.lp
 ./replace.rb rok4.txt out/z_poznaniem_z_danymi.lp out/zp4.lp
 ./replace.rb rok5.txt out/z_poznaniem_z_danymi.lp out/zp5.lp
+./replace.rb rok6.txt out/z_poznaniem_z_danymi.lp out/zp6.lp
+./replace.rb rok7.txt out/z_poznaniem_z_danymi.lp out/zp7.lp
+./replace.rb rok8.txt out/z_poznaniem_z_danymi.lp out/zp8.lp
+./replace.rb rok9.txt out/z_poznaniem_z_danymi.lp out/zp9.lp
+./replace.rb rok10.txt out/z_poznaniem_z_danymi.lp out/zp10.lp
